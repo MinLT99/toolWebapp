ebecd4784e79050005e362fa39d41885b666a2e4
minhluan <minhluanthuda42@gmail.com>
2024-06-27 10:04:33 +0700

    cập nhật 2 ile pagaket.json  và pagaketlock.json

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter } from 'react-router-dom';
-import App from './app';
+import  App  from './app.jsx';
 
 // ----------------------------------------------------------------------
 
